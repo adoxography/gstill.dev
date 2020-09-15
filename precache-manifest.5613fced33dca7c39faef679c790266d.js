3cad6ec4b9974d21f4bb3b47988aca4ead742497
adoxography <adoxography@users.noreply.github.com>
2020-09-15 22:55:22 +0000

    deploy: 418665fdbabc73598d97badebd26b86aceded56e

---
+++ precache-manifest.5613fced33dca7c39faef679c790266d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a480d6a02c57a1e395d19df38a01246c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aadf3dd631c840ab40a",
+    "url": "/static/css/main.eb17fd4c.chunk.css"
+  },
+  {
+    "revision": "e9c1b5356a86f36c1eed",
+    "url": "/static/js/2.762df084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.762df084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aadf3dd631c840ab40a",
+    "url": "/static/js/main.2ed9142f.chunk.js"
+  },
+  {
+    "revision": "f897361a250703153df3",
+    "url": "/static/js/runtime-main.85ea43e0.js"
+  },
+  {
+    "revision": "9877b06f6d0b8158690c103066d78f48",
+    "url": "/static/media/alglang.9877b06f.svg"
+  },
+  {
+    "revision": "c23305680e44962e2cec78a0c2f1e300",
+    "url": "/static/media/pixels.c2330568.png"
+  },
+  {
+    "revision": "22d21d4577e9fef4c280dbbb4e22d612",
+    "url": "/static/media/robot.22d21d45.svg"
+  }
+]);
