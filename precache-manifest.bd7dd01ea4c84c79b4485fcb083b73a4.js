02df2c8748a4c494e6ff961a672ad570ce8b5c66
adoxography <adoxography@users.noreply.github.com>
2020-09-15 18:31:23 +0000

    deploy: 625d17eae4ced750ced92c0874a1f664ed9c3d58

---
+++ precache-manifest.bd7dd01ea4c84c79b4485fcb083b73a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9663b0d2931445a8b2823973061e34c",
+    "url": "/alglang.dev/index.html"
+  },
+  {
+    "revision": "5baeeae7a8a3f65c53a8",
+    "url": "/alglang.dev/static/css/main.b433b417.chunk.css"
+  },
+  {
+    "revision": "537d5e4a5ac0d3e9e3d0",
+    "url": "/alglang.dev/static/js/2.9ea747d9.chunk.js"
+  },
+  {
+    "revision": "5baeeae7a8a3f65c53a8",
+    "url": "/alglang.dev/static/js/main.523fa040.chunk.js"
+  },
+  {
+    "revision": "9f00020ee553c704abb9",
+    "url": "/alglang.dev/static/js/runtime-main.e1b68736.js"
+  },
+  {
+    "revision": "c23305680e44962e2cec78a0c2f1e300",
+    "url": "/alglang.dev/static/media/pixels.c2330568.png"
+  },
+  {
+    "revision": "22d21d4577e9fef4c280dbbb4e22d612",
+    "url": "/alglang.dev/static/media/robot.22d21d45.svg"
+  }
+]);
