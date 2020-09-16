17821a74756f445b4ba5dc6cb2c9f1df8c84e534
Graham Still <gstill@uw.edu>
2020-09-15 21:15:08 -0500

    Brighten portfolio preview text

--- src/components/Home/Portfolio.js
+++ src/components/Home/Portfolio.js
@@ -24,7 +24,7 @@ const PortfolioItem = ({ value, onClick }) => {
           className="transform-all duration-300 ease-in-out transform group-hover:scale-150 group-hover:opacity-25 group-focus:scale-150 group-focus:opacity-25 min-w-full h-full object-cover bg-gray-400"
         />
 
-        <div className="absolute p-6 z-30 top-0 bottom-0 left-0 right-0 justify-center items-center transform  group-hover:translate-y-0 group-focus:translate-y-0 transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 group-focus:opacity-100 text-gray-300">
+        <div className="absolute p-6 z-30 top-0 bottom-0 left-0 right-0 justify-center items-center transform  group-hover:translate-y-0 group-focus:translate-y-0 transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 group-focus:opacity-100 text-gray-100">
           <p className="font-semibold font-display text-4xl leading-none mb-4">
             {title}
           </p>
