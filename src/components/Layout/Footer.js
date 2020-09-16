15f0a61dff1530baf90843ef5a2e9a63123460b6
Graham Still <gstill@uw.edu>
2020-09-16 15:28:08 -0500

    Add default link hover style

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const SocialLink = ({ name,  href, cls }) => (
-  <a href={href} target="_blank" rel="noopener noreferrer" className="p-2 hover:opacity-75" aria-label="name">
+  <a href={href} target="_blank" rel="noopener noreferrer" className="p-2" aria-label="name">
     <i className={`fab ${cls}`}></i>
   </a>
 );
 
 const Footer = () => (
-  <footer className="bg-gray-900 text-indigo-600 text-center py-10 text-2xl">
-    <a href="mailto:graham@gstill.dev" className="font-semibold hover:opacity-75">
+  <footer className="bg-gray-900 text-center py-10 text-2xl">
+    <a href="mailto:graham@gstill.dev" className="font-semibold">
       graham@gstill.dev
     </a>
 
